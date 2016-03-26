cf5ad0365579a4420574eaed2c7d5a220de7a8b8
Jacob Frank <frankjacob42@gmail.com>
2016-03-25 23:32:25 -0400

    completed header
    Added current version of website, in this version the non-mobile ready header was created. Also added a css file for the current styling.

---
+++ header.php
@@ -0,0 +1,14 @@
+<header class="banner">
+        <h1>
+            <img src="images/TAW_Logo.svg" alt="Thought At Work Logo" style="width:30px;">
+            <a href="" rel="home" class = "logotype">&nbsp;THOUGHT AT WORK</a>
+        </h1>
+        <nav role="navigation">
+            <a href="">READY UP</a>
+            <a  class = "dotLeft">&middot;</a>
+            <a href="">ABOUT US</a>
+            <a class = "dotLeft" >&middot;</a>
+            <a href="">CONTACT&nbsp;</a>
+            <a class = "register" href="">REGISTER</a>
+        </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Thought At Work - Home</title>
+  <meta name="description" content="Our Website">
+  <meta name="author" content="Thought at Work">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/main.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+	</head>
+	<body>
+
+		<div>
+			<!--- First background block -->
+		</div>
+
+		<div>
+			<?php include("header.php"); ?>
+			<div>
+			<!--- Header thing -->
+			</div>
+			<div>
+			<!--- 2015 Recap -->
+			</div>
+		</div>
+
+		<div>
+			<!--- Second background block -->
+		</div>
+
+		<div>
+			<!--- Second content block -->
+			<div>
+			<!--- Come Hang -->
+
+			</div>
+		</div>
+
+		<div>
+			<!--- Third content block -->
+			<div>
+			<!--- Featured Speackers -->
+			</div>
+			<div>
+			<!--- Sponsors -->
+			</div>
+
+		</div>
+
+		<!--- Footer Inlcude-->
+	</body>
+</html>
