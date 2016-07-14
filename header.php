00a785de3a7785d63687f1bc01666ee77eadc717
JacobDFrank <frankjacob42@gmail.com>
2016-07-13 20:43:09 -0400

    Worked on spacing and the hamburger menu

--- header.php
+++ header.php
@@ -1,14 +1,22 @@
-<nav class="navbar transparent navbar-fixed-top p-a-9 p-t-3">
-  <div class="navbar-inner container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand title" href="#">Thought at Work</a>
-    </div>
-    <ul class="nav navbar-nav navbar-right p-a-4">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Arrival</a></li>
-      <li><a href="#">About Us</a></li>
-      <li><a href="#">Contact</a></li>
-      <li><a href="#">Register</a></li>
-    </ul>
-  </div>
-</nav>
+<nav class="navbar navbar-fixed-top p-a-9 p-t-3" role="navigation">
+ <div class="container">
+   <div class="navbar-header">
+             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                 <span class="sr-only">Toggle navigation</span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+           </button>
+     <a class="navbar-brand title p-l-12" href="#">Thought at Work</a>
+   </div>
+         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+             <ul class="nav navbar-nav navbar-right p-r-9">
+               <li class="active"><a href="#">Home</a></li>
+               <li><a href="#">Arrival</a></li>
+               <li class="text-nowrap"><a href="#">About Us</a></li>
+               <li><a href="#">Contact</a></li>
+               <li><a href="#">Register</a></li>
+             </ul>
+         </div>
+     </nav>
+ </div>
