a9d7a505005f676981e2b830bfd09dac29b9359f
JacobDFrank <frankjacob42@gmail.com>
2016-07-16 00:24:04 -0400

    Footer almost done sans dot and line

--- index.php
+++ index.php
@@ -35,8 +35,31 @@
 
 	</head>
 	<body>
-			 <?php include("header.php"); ?> 
-	
+			 <!-- <?php include("header.php"); ?>  -->
+			 <nav class="navbar navbar navbar-fixed-top p-a-9 p-t-3" role="navigation">
+	                 <div class="container-fluid">
+	 			   <div class="navbar-header">
+	 			             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#expand-nav" aria-expanded="false">
+	 			                 <span class="sr-only">toggle navigation</span>
+	 			                 <span class="icon-bar"></span>
+	 			                 <span class="icon-bar"></span>
+	 			                 <span class="icon-bar"></span>
+	 			           </button>
+	 			     <a class="navbar-brand title p-l-12" href="#">Thought at Work</a>
+	 			   </div>
+	 			         <div class="collapse navbar-collapse navbar-inner" id="expand-nav">
+	 			             <ul class="nav navbar-nav navbar-right p-r-9">
+	 			               <li class="active"><a href="#">Home</a></li>
+	 			               <li><a href="#">Arrival</a></li>
+	 			               <li class="text-nowrap"><a href="#">About Us</a></li>
+	 			               <li><a href="#">Contact</a></li>
+	 			               <li><a href="#">Register</a></li>
+	 			             </ul>
+	 			         </div>
+	                     </div>
+	                  </nav>
+	 			 
+	<!--  -->
 		<!-- HEADER / SPASH -->
 		<div class="container-fluid splash-words">
 			<div class="row">
@@ -102,12 +125,12 @@
 			</div>
 			<div class="row">
 				<div class="col-lg-6">
-						
+
 						<div class="come">
 							<h4>Who Should Come</h4>
 
 						<p>Thought At Work is a design conference centered around industrial, graphic, motion, 3d, and interactive designers and developers. Our workshops and events are primarily student-focused, but we welcome all graduates, high school students, pros, vets, dabblers, and design moms.</p>
-				
+
 						</div>
 				</div>
 			</div>
@@ -156,5 +179,7 @@
 		<!--- Footer Include-->
 		<?php include("footer.php"); ?>
 
+        
+        <!---->
 	</body>
 </html>
