4b077fb16c7624c02aa224bf3b037f103afcecca
colejohnson <cjinak810@gmail.com>
2016-08-10 22:23:17 -0400

    This menu is a bundle of fun

--- js/main.js
+++ js/main.js
@@ -49,3 +49,178 @@ function initializeClock(id, endtime){
 }
 
 initializeClock('clockdiv', deadline);
+
+
+function campus(){
+
+    document.getElementById("food-menu-2").scrollTop = 0;
+    document.getElementById("food-menu-list2").innerHTML = "";
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Java’s");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Midnight Oil");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Crossroads");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Salsarita’s");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Lovin’ Cup");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+
+}
+
+function henrietta(){
+
+    document.getElementById("food-menu-2").scrollTop = 0;
+    document.getElementById("food-menu-list2").innerHTML = "";
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Jay’s Diner");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Bruegger’s");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Pi Craft");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("MacGregor’s");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("SEA Restaurant");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Chipotle");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Raj Mahal");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Wegmans");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Sticky Lips");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+
+}
+
+
+function rochester(){
+
+    document.getElementById("food-menu-2").scrollTop = 0;
+    document.getElementById("food-menu-list2").innerHTML = "";
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Tap & Mallet");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Swillburger");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("The Owl House");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Salena’s");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Dogtown");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Dinosaur BBQ");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("ButaPub");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Lux");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Nox");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Solera Wine Lounge & Cheshire Bar");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+    var listItem = document.createElement('li');
+    listItem.className = "food-menu-list2-item";
+    var text = document.createTextNode("Marshall Street Bar & Grill");
+    listItem.appendChild(text);
+    document.getElementById("food-menu-list2").appendChild(listItem);
+
+}
