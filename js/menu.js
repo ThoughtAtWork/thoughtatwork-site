272bdecdf3d65515bd4218242dedb7173e9dd8ae
colejohnson <cjinak810@gmail.com>
2016-08-13 10:59:53 -0400

    Final push for awhile

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,20 @@
  * Created by colejohnson on 8/12/16.
  */
 
+
+function gofetch(name){
+    alert("hello");
+    var k;
+    for (k = 0; k < restaurants.length; ++k) {
+        if(restaurants.name == name){
+            alert(name.desc)
+        }
+    }
+
+}
+
+
+
 function campus(){
 
     document.getElementById("food-menu-2").scrollTop = 0;
@@ -10,6 +24,7 @@ function campus(){
     var listItem = document.createElement('li');
     listItem.className = "food-menu-list2-item";
     var text = document.createTextNode("Java’s");
+    listItem.addEventListener("click", gofetch("Java's"));
     listItem.appendChild(text);
     document.getElementById("food-menu-list2").appendChild(listItem);
 
@@ -175,3 +190,22 @@ function rochester(){
     document.getElementById("food-menu-list2").appendChild(listItem);
 
 }
+
+var restaurants = {
+    "Java's": {
+        "desc": "Enjoy fresh gourmet coffee, tea, smoothies, lemonade and light snacks while relaxing in a casual, comfortable atmosphere.",
+        "location": "Located on the 1st floor of the Wallace Library",
+        "hours": "Friday: 7:30AM -10:30PM <br> Saturday: 11:30AM - 9:00PM <br> Sunday: 11:30AM - 9:00PM"
+    },
+    "Midnight": {
+        "desc": "Convenient destination open late serving everything from breakfast sandwiches and Starbucks coffee and tasty artisan sandwiches and treats.",
+        "location": "Located in Global Village",
+        "hours": "Friday: 7:30AM -12:00AM<br/>Saturday: 10:00AM - 12:00AM</br>Sunday: 10:00AM - 12:00AM"
+    },
+    "Crossroads": {
+        "desc": "Market and cafe with stations featuring pasta & pizza, salads & to-go, a deli, Jump Asian Cuisine, Cross Bar, and the grill.",
+        "location": "Located in Global Village",
+        "hours": "Friday: 11:00AM- 11:00PM <br/>Saturday: Closed</br>Sunday: 12:00PM - 8:00PM"
+    }
+};
+
