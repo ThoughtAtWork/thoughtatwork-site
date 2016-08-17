0a6888370affb6bce86a12b27e61a2b22a7704b8
Conner <conner.hasbrouck@troyweb.com>
2016-08-17 11:23:31 -0400

    corrected mobile black nav

--- js/navScript.js
+++ js/navScript.js
@@ -1,10 +1,35 @@
 var navOffset = $(".navOff").offset().top;
 
-$(document).scroll(function(){
-    if($(this).scrollTop() > navOffset)
-    {   
-       $('.nav').css({"background":"black"});
-    } else {
-       $('.nav').css({"background":"transparent"});
-    }
-});
+
+    
+    
+		$(document).scroll(function(){
+		    if($(this).scrollTop() > navOffset)
+		    {   
+		    	
+			       $('.nav').css({"background":"black"});
+			   
+		    } else {
+		       $('.nav').css({"background":"transparent"});
+		    }
+		});
+	
+		$(document).scroll(function(){
+			var viewportWidth = $(window).width();
+			if( viewportWidth < 768)
+			{
+				$('.nav').css({"background":"transparent"});
+			}
+		});
+
+		$(window).resize(function () {
+			var viewportWidth = $(window).width();
+			if( viewportWidth < 768)
+			{
+				$('.nav').css({"background":"transparent"});
+			}
+			if( viewportWidth > 768)
+			{
+				$('.nav').css({"background":"black"});
+			}
+		});
