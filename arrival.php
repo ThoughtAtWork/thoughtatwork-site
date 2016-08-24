bf7fdda5f8a5cce52f593d5a77b13fa2301245f3
JacobDFrank <frankjacob42@gmail.com>
2016-08-23 21:55:14 -0400

    Fixed arrival nav

--- arrival.php
+++ arrival.php
@@ -28,6 +28,10 @@
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
@@ -279,5 +283,6 @@
                             <!--- Footer Include-->
                         <?php include("footer.php"); ?>
                                         <!---->
+			<script type="text/javascript" src="js/navScript.js"></script>
     </body>
 </html>
