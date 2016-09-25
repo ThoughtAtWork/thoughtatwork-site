6dc931b7f1629673f634c88cf7bbe2a4e5e58a11
Conner <conner.hasbrouck@troyweb.com>
2016-09-25 12:32:28 -0400

    anto orphan jquery

--- footer.php
+++ footer.php
@@ -76,3 +76,10 @@
 </div>
 </div>
 </footer>
+<script type="text/javascript">
+    $('p').each(function(){
+    var string = $(this).html();
+    string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+    $(this).html(string);
+});
+</script>
