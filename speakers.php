86a4e60bc5a1f348b0c494e51c9bce86153e9c49
Conner <conner.hasbrouck@troyweb.com>
2016-10-05 20:50:20 -0400

    speaker page init

---
+++ speakers.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Thought At Work - About Us</title>
+
+	<!-- Open Graph data -->
+	<meta property="og:url"                content="http://thoughtatwork.cias.rit.edu" />
+	<meta property="og:type"               content="article" />
+	<meta property="og:title"              content="Thought At Work, a student-run design conference" />
+	<meta property="og:description"        content="Thought At Work is a three-day, student-run, student-focused design conference that takes place every October at Rochester Institute of Technology." />
+	<meta property="og:image"              content="http://thoughtatwork.cias.rit.edu/img/tawtemp_share_image.jpg" />
+
+	<!-- Twitter Card data -->
+	<meta name="twitter:card" content="product">
+	<meta name="twitter:site" content="@TAW_RIT">
+	<meta name="twitter:title" content="Thought At Work, a student-run design conference">
+	<meta name="twitter:description" content="Thought At Work is a three-day, student-run, student-focused design conference that takes place every October at Rochester Institute of Technology.">
+	<meta name="twitter:creator" content="@TAW_RIT">
+	<meta name="twitter:image" content="http://www.example.com/image.jpg">
+
+	<meta name="description" content="Student-Run Design Conference">
+	<meta name="title" content="Thought at Work">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/main.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="img/favicon.png">
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<!-- <script src="../constantlyUpdatingScript.js"></script> -->
+
+	</head>
+	<body>
+        <?php include("header.php"); ?>
+ <!--  -->
+		<!-- HEADER / SPLASH -->
+		<div class="container-fluid about-splash-words ">
+			<div class="row">
+				<div class="col-lg-12 ">
+					<h1 class="title ">Speaker</h1>
+				</div>
+			</div>
+		</div>
+     <div class="navOff"></div>
+
+			<!-- COME HANG -->
+		<div class="container-fluid top-buffer about-mobile-top">
+			<div class="row about-margins">
+				<div class="col-lg-10 col-md-offset-1 mobile-about-history">
+						<h2 class="elliot about-history-padding ">Speakers</h2>
+				</div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+				<div class="col-md-3 col-md-offset-2">
+						<img src="img/speaker.jpg" class="img-responsive"></img>
+				</div>
+        <div class="col-md-5">
+            <h2 class="speaker-h2">Mount Mograph</h2>
+            <h3 class="speaker-h3">DCI-Artform</h3>
+            <h4 class="speaker-h4">Creative Coding, Photography, Development</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est</p>
+        </div>
+
+			</div>
+    </div>
+    
+        <!--- Footer Include-->
+
+		<?php include("footer.php"); ?>
+       <script type="text/javascript" src="js/navScript.js"></script>
+	</body>
+</html>
