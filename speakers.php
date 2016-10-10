2d14cabdf036bdfd0c59f9f6f4103ecb1292d3f7
JacobDFrank <frankjacob42@gmail.com>
2016-10-10 00:52:36 -0400

    speakers styles work

--- speakers.php
+++ speakers.php
@@ -61,7 +61,7 @@
 		<div class="container-fluid about-splash-words ">
 			<div class="row">
 				<div class="col-lg-12 ">
-					<h1 class="title ">Speaker</h1>
+					<h1 class="title ">Speakers</h1>
 				</div>
 			</div>
 		</div>
