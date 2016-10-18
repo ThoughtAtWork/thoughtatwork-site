2dab9e11bbd5a833c66949259f790d41bcff8f9c
JacobDFrank <frankjacob42@gmail.com>
2016-10-18 17:16:45 -0400

    Moved schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -14,20 +14,20 @@ var schedule = {
         disc: ""
       },
       {
-        title: "PTC Round Table",
-        person: "Wendell Castle",
+        title: "Product Timecapsule: Round Table Discussion With Wendell Castle",
+        person: "Josh Owen & Wendell Castle",
         loc: "Vignelli",
         start: "10:00AM",
         end: "11:00AM",
         disc: ""
       },
       {
-        title: "Opening Address & Title Sequence",
+        title: "Opening Address",
         person: "",
         loc: "Webb Auditorium",
         start: "11:45AM",
         end: "12:30PM",
-        disc: ""
+        disc: "Welcome to Thought At Work! The opening address will have announcements, an introduction to the events of the weekend, and an awesome title sequence."
       },
       {
         title: "Crossing Plateaus",
@@ -46,12 +46,12 @@ var schedule = {
         disc: "Where do ideas come from? In this candid talk, designer and RIT professor Mitch Goldstein takes us on a journey through the past 44 years of his life and examines the art he saw, the movies he watched, the music he listened to, and the books he read — and how these things (might) have influenced his work."
       },
       {
-        title: "Title Coming Soon",
+        title: "The Entrepreneurial Spirit of Design",
         person: "Jordan Nollman",
         loc: "Booth 3550",
         start: "12:45PM",
         end: "1:45PM",
-        disc: "Description coming soon..."
+        disc: "Jordan will be showcasing some of Sprout's new and old ventures, as well sharing the pros and cons of these endeavors."
       },
       {
         title: "Title Coming Soon",
@@ -62,12 +62,12 @@ var schedule = {
         disc: "Description coming soon"
       },
       {
-        title: "Friday Keynote Presentation: Designing A Better Mouse Trap",
+        title: "Friday Keynote Presentation – Design: A User Experience",
         person: "Patricia Moore",
         loc: "Webb Auditorium",
         start: "2:00PM",
         end: "3:30PM",
-        disc: "Creating solutions for medication management."
+        disc: "How Design, with a capitol \"D\" defines the quality of our Lifespan. RIT Alum Pattie Moore, named One of the 40 Most Socially Rsponsible Designers in the World, One of the 100 Most Important Women in America and One of the 50 People Defining this Century, discusses how Designers determine the dreams of our lives."
       },
       {
         title: "Truth Or Dare",
@@ -120,26 +120,26 @@ var schedule = {
       {
         title: "Digital Painting Workshop",
         person: "Jasmin Lai",
-        loc: "Booth 3330",
+        loc: "Booth 1480",
         start: "5:00PM",
         end: "6:30PM",
         disc: "As a digital artist, Jasmin specializes in more of a graphic design style than the painterly, sci-fi style that you often see in the industry. In this digital painting workshop, Jasmin will show you how she creates a piece from start to finish, while explaining how her technique might vary when she’s working for different projects such as Steven Universe."
       },
       {
         title: "Intro to KeyShot",
-        person: "Will Gibbons",
+        person: "Will Gibbons + KeyShot",
         loc: "Booth 3550",
         start: "5:00PM",
         end: "6:30PM",
         disc: "What is KeyShot? How do you use it? Will Gibbons will give you an overview of this advanced rendering software, so you can be ready to take it to the next level."
       },
       {
-        title: "Sketch Off ",
+        title: "Sketch-Off",
         person: "",
         loc: "Booth 3310, 3330, 3340",
         start: "7:00PM",
         end: "8:15PM",
-        disc: "Dinner Provided."
+        disc: "It’s back. Load up your plate and prepare to witness the most intense hour of sketching you’ve ever seen. Friday night, the specially drafted group of creative contenders will flex their artistic muscles as they compete for the coveted Sketchoff prize."
       },
       {
         title: "After Party",
@@ -147,7 +147,7 @@ var schedule = {
         loc: "Lovin Cup",
         start: "9:00PM",
         end: "12:00AM",
-        disc: "Address: 300 Park Point Dr, Rochester, NY 14623."
+        disc: "Kick back Friday night at this nearby bar and restaurant. We hear there’s going to be a live band and your cover charge is on us (just flash your badge). Here's the address: 300 Park Point Dr, Rochester, NY 14623."
       }
     ],
   },
@@ -162,12 +162,12 @@ var schedule = {
         disc: ""
       },
       {
-        title: "Breakfast - Bagels & Coffee",
+        title: "Breakfast",
         person: "",
         loc: "Booth 2nd Floor Lobby",
         start: "9:00AM",
         end: "11:00AM",
-        disc: ""
+        disc: "Bagels & Coffee"
       },
       {
         title: "Portfolio Review",
@@ -175,7 +175,7 @@ var schedule = {
         loc: "University Gallery",
         start: "9:30AM",
         end: "11:00AM",
-        disc: ""
+        disc: "You’ve got a portfolio, we’ve got feedback. By filling a room with portfolio wizards (working professionals), we provide a venue to discuss and be taught how to craft a portfolio that’ll knock the socks off anyone who sees it. Bring your work!"
       },
       {
         title: "Title coming soon",
@@ -187,7 +187,7 @@ var schedule = {
       },
       {
         title: "KeyShot: Advanced Features",
-        person: "Will Gibbons",
+        person: "Will Gibbons + KeyShot",
         loc: "Gannett 1217",
         start: "9:30AM",
         end: "11:00AM",
@@ -206,7 +206,7 @@ var schedule = {
         person: "Maya Bruck",
         loc: "Booth 1480",
         start: "11:15AM",
-        end: "12:15AM",
+        end: "12:15PM",
         disc: "Designing for the web is complicated, and your process shouldn't make it harder. Maya will share tools and techniques for engaging your team in the creative process – turning research, prototyping, design, and development into a collaborative and highly effective effort."
       },
       {
@@ -214,7 +214,7 @@ var schedule = {
         person: "Matt Olpinski",
         loc: "Booth 1540",
         start: "11:15AM",
-        end: "12:15AM",
+        end: "12:15PM",
         disc: "Coding your own website doesn’t have to be difficult. In this workshop, Matt will explain HTML & CSS in a way that will get you excited by revealing its underlying simplicity. When you can bring your designs to life through code, you’ll have a competitive advantage and a rare superpower that enables you to create almost anything for the web."
       },
       {
@@ -222,7 +222,7 @@ var schedule = {
         person: "Chris Remo",
         loc: "Booth 3310",
         start: "11:15AM",
-        end: "12:15AM",
+        end: "12:15PM",
         disc: "How do you tell a compelling and interactive story in a game—without using combat, mechanical challenge, or adventure game puzzles? Increasingly, developers are answering this question in their own ways. Chris will share the approach Campo Santo stumbled into with its debut game, Firewatch."
       },
       {
@@ -230,19 +230,19 @@ var schedule = {
         person: "Helen Androlia",
         loc: "Booth 3340",
         start: "11:15AM",
-        end: "12:15AM",
+        end: "12:15PM",
         disc: "If you think that a job title like ‘Social Interaction Strategist’ sounds completely made up, that’s okay – it was. In fact, it was made up for Helen Androlia. In the quickly evolving digital landscape everything is rewritten on a daily basis, and job roles are no different. Some futurists predict that in as little as under two decades over 2 billion jobs will become extinct, and while that sounds like a terrifying statistic, it also opens up opportunities that haven’t even been created yet – especially if you’re ready to build your own. Helen examines how you too can create your own job title with case studies from working professionals who also built their roles from scratch, and develops a potential framework for how transitioning professionals and students entering the workforce might just be able to build their own dream digital and social job."
       },
       {
         title: "Lunch",
         person: "",
         loc: "",
-        start: "12:30AM",
+        start: "12:30PM",
         end: "1:30PM",
-        disc: ""
+        disc: "It’s lunch! You know, like eating food."
       },
       {
-        title: "Keynote Opener: Killing the Cat",
+        title: "Keynote Opener – Killing the Cat",
         person: "Allan Chochinov + Core77",
         loc: "Ingle Auditorium",
         start: "1:45PM",
@@ -250,7 +250,7 @@ var schedule = {
         disc: "Should design ever hold back? As our keynote opener, Allan Chochinov (Partner at Core77) will explore the role and limits of design in areas that make often make people uncomfortable – but maybe that’s exactly where design is needed the most."
       },
       {
-        title: "Saturday Keynote Presentation: Stuff I Did",
+        title: "Saturday Keynote Presentation – Stuff I Did",
         person: "Jake Lodwick",
         loc: "Ingle Auditorium",
         start: "2:30PM",
@@ -282,12 +282,12 @@ var schedule = {
         disc: "Description coming soon."
       },
       {
-        title: "Title Coming Soon.",
+        title: "Building A Better Mousetrap: AGEing By Design",
         person: "Patricia Moore",
         loc: "Booth 3330",
         start: "4:15PM",
         end: "5:45PM",
-        disc: "Description coming soon."
+        disc: "How Design defines the wishes, wants and needs of all consumers for th‎eir entire lifespan."
       },
       {
         title: "Bowling!",
@@ -295,19 +295,19 @@ var schedule = {
         loc: "Bowl-A-Roll",
         start: "9:00PM",
         end: "10:30PM",
-        disc: "Sponsored by Core77. Address: 1560 Jefferson Rd, Rochester, NY 14623."
+        disc: "We’re trying something new this year – bowling! Come hang out, play a few games, and connect with other designers. Huge thanks to Core77 for making this event possible. Address: 1560 Jefferson Rd, Rochester, NY 14623."
       }
     ]
   },
   "sun": {
     events: [
       {
-        title: "Breakfast - Coffee & Cereal",
+        title: "Breakfast",
         person: "",
         loc: "Booth 2nd Floor Lobby",
         start: "10:00AM",
         end: "11:30AM",
-        disc: ""
+        disc: "Coffee & Cereal (not mixed together)"
       },
       {
         title: "Kickstart Your Freelancing Career",
@@ -315,18 +315,18 @@ var schedule = {
         loc: "Booth 1540",
         start: "11:30AM",
         end: "12:30PM",
-        disc: "When you’re just starting out, freelancing can seem intimidating and overwhelming. Where do you find clients? How much do you charge? How can you guarantee payment? What if you don’t get consistent work? In this workshop, Matt will teach you how to become a rockstar freelancer in a saturated and competitive market. You’ll learn how to attract your ideal clients, write winning proposals, charge for value, and kickstart a successful freelancing business."
+        disc: "When you’re just starting out, freelancing can seem overwhelming. Where do you find clients? How much do you charge? How can you guarantee payment? What if you don’t get consistent work? In this workshop, Matt will teach you how to become a rockstar freelancer in a saturated and competitive market. You’ll learn how to attract your ideal clients, write winning proposals, charge for value, and kickstart a successful freelancing business."
       },
       {
-        title: "Title Coming Soon",
+        title: "Self Care For Artists",
         person: "Susanna Rose",
         loc: "Booth 3540",
         start: "11:30AM",
         end: "1:00PM",
-        disc: "Description coming soon"
+        disc: "Conferences like this usually focus on software tools, production techniques, and which lines of code to write. But life as a designer gets hectic, even overwhelming…so how do we handle the demands of our work while still making time to nurture ourselves and our creativity? In this experiential and discussion-based session, Susanna Rose will focus on topics like mindfulness, self-compassion, non-judgment, and play. This workshop was born out Susanna’s own experiences as an artist and her training as a Mental Health Counselor at St. John Fisher College."
       },
       {
-        title: "Title Coming Soon",
+        title: "Hand-Sketching Workshop",
         person: "Jeff Smith",
         loc: "Booth 3310",
         start: "11:30AM",
@@ -334,12 +334,12 @@ var schedule = {
         disc: "Description coming soon."
       },
       {
-        title: "Sunday Panel",
+        title: "Lazy Sunday: A Panel Discussion",
         person: "",
         loc: "Booth 3310",
         start: "1:15PM",
         end: "2:15PM",
-        disc: "Description coming soon."
+        disc: "Description coming soon"
       },
       {
         title: "Closing Address",
