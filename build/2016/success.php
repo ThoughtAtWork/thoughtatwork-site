fcfd0430709702068632516c84ec3fb4eaa9c098
JacobDFrank <frankjacob42@gmail.com>
2017-06-30 01:08:57 -0400

    started to refactor php on 2016 site to html

--- build/2016/success.php
+++ build/2016/success.php
@@ -238,7 +238,60 @@
 
 	</head>
 	<body>
-        <?php include("header.php"); ?>
+        <!--Web Team
+
+    Spiritual Leader:Jacob Frank
+    Moral Support: Cole Johnson
+    Loopy Lady: Amy Pham
+    Dude With Beard: Conner Hasbrouck
+
+    In Loving Memory of Sara Artese
+-->
+<nav id="nav-background-color" class="navbar navbar-fixed-top p-a-9 nav" role="navigation">
+    <div class="container-fluid navColorChange">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand title p-l-12 mobile-hide" href="index.php"><div class="tempHover">Thought at Work</div></a>
+        </div>
+        <div id="menu" class="navbar-collapse collapse navbar-inner pull-right drop-down" >
+            <span class="x">
+            <button type="button" class="navbar-toggle exit" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <i class="close" aria-hidden="true"></i>
+            </button>
+            </span>
+            <ul class="nav navbar-nav navbar-right p-r-9 mobile-center menu-pad">
+                <li class="mobile-home"><a href="index.php">Home</a></li>
+                <li><a href="speakers.php">Speakers</a></li>
+                <li><a href="schedule.php">Schedule</a></li>
+                <li class="text-nowrap"><a href="getready.php">Get Ready</a></li>
+                <li class="text-nowrap"><a href="about.php">About Us</a></li>
+               <li><a href="contact.php">Contact</a></li>
+               <li><a class="nav-register menu-pad" href="register.php">Register</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!--Underline effect for current page in nav-->
+<script>
+    $('.nav').find('a').each(function(){
+        var href = document.location.href;
+        var fileName = href.substr(href.lastIndexOf('/') + 1);
+        if($(this).attr('href') == fileName && fileName != "index.php"){
+            if(fileName == "register.php")
+                $(this).addClass('currentRegister');
+            else
+                $(this).addClass('currentPage');
+        }
+    });
+</script>
+
  <!--  -->
 		<!-- HEADER / SPLASH -->
 		<div class="container-fluid about-splash-words">
@@ -283,7 +336,120 @@
 
     <script src="js/contactScript.js"></script>
         <!--- Footer Include-->
-		<?php include("footer.php"); ?>
+		<!-- SPONSORS -->
+<div class="container-fluid sponsors tablet-top">
+<div class="row sponsor-top">
+<!--Because tiny fish is currently not a spnsor they've been removed but this code is still here just in case-->
+<div class="center-mid">
+<div class="col-sm-3 firstRow-sponsor-images">
+    <a href="https://www.keyshot.com/" target="_blank">
+   <img class="sponsor-img" src="img/sponsor-logos-01.png" />
+    </a>
+        </div>
+<div class="col-sm-3">
+    <a href="http://www.autodesk.com" target="_blank">
+   <img class="sponsor-img" src="img/sponsor-logos-02.png" />
+    </a>
+</div>
+<div class="col-sm-3  firstRow-sponsor-images">
+    <a href="http://www.core77.com/" target="_blank">
+   <img class="sponsor-img" src="img/sponsor-logos-03.png" />
+    </a>
+</div>
+<div class="col-sm-3">
+    <a href="http://fitc.ca/" target="_blank">
+   <img class="sponsor-img" src="img/sponsor-logos-04.png" />
+    </a>
+</div>
+</div>
+</div>
+<div class="row sponsor-top mobile-remove-sponsor-top">
+<div class="center-mid">
+   <div class="col-sm-3 firstRow-sponsor-images">
+       <a href="http://www.eastman.com/" target="_blank">
+           <img class="sponsor-img" src="img/sponsor-logos-05.png" />
+        </a>
+   </div>
+<div class="col-sm-3">
+   <a href="https://www.raymondcorp.com/" target="_blank">
+   <img class="sponsor-img" src="img/sponsor-logos-06.png" />
+    </a>
+</div>
+<div class="col-sm-3  firstRow-sponsor-images">
+   <a href="http://www.wacom.com/" target="_blank">
+   <img class="sponsor-img" src="img/sponsor-logos-07.png" />
+    </a>
+</div>
+<div class="col-sm-3">
+    <a href="http://cias.rit.edu/" target="_blank">
+   <img class="sponsor-img" src="img/sponsor-logos-08.png" />
+    </a>
+</div>
+</div>
+</div>
+<div class="row sponsor-top mobile-remove-sponsor-top">
+<div class="center-mid">
+   <div class="col-sm-3 firstRow-sponsor-images">
+       <a href="http://industrialdesign.cias.rit.edu/community/idsa-student-chapter/" target="_blank">
+           <img class="sponsor-img" src="img/sponsor-logos-09.png" />
+        </a>
+   </div>
+<div class="col-sm-3">
+   <a href="http://www.tinyfishprinting.com/" target="_blank">
+           <img class="sponsor-img" src="img/sponsor-logos-10.png" />
+    </a>
+</div>
+<div class="col-sm-3  firstRow-sponsor-images">
+   <a href="https://www.stickermule.com/" target="_blank">
+       <img class="sponsor-img" src="img/sponsor-logos-11.png" />
+    </a>
+</div>
+<div class="col-sm-3">
+    <a href="http://www.javascafe.com/" target="_blank">
+       <img class="sponsor-img" src="img/sponsor-logos-12.png" />
+    </a>
+</div>
+</div>
+</div>
+<div class="row tablet-bot">
+<div class="col-lg-12 sponsor-want center-mid">
+<div class="row-spacer">&nbsp;</div>
+<a href="contact.php" class="elliot-bold dark link-effect">WANT TO SPONSOR?</a>
+</div>
+</div>
+</div>
+<!-- FOOTER -->
+<footer class="tablet-top">
+<div class="container footer-nav navbar-inner">
+<div class="row">
+<div class="col-sm-10 col-md-offset-1 text-center">
+   <!-- <div class="col-sm-2"><a href="schedule.php"  class="link-effect">SCHEDULE</a></div> -->
+   <div class="col-sm-2"><a href="getready.php" class="link-effect">GET READY</a></div>
+   <div class="col-sm-2"><a href="register.php" class="link-effect">REGISTER</a></div>
+   <div class="col-sm-2 text-nowrap footer-logo-mobile"><a href="contact.php" class="link-effect">GET IN TOUCH</a></div>
+   <div class="col-sm-2 tablet-hide">
+       <div class="circle-footer"></div>
+       <div class="line-footer"></div>
+   </div>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="container footer-text">
+<div class="footer-logo"></div>
+<p class="special">Special thanks to Lorraine Justice, Josh Owen, Bruce Leonard, Adam Smith, and Benjamin Thomas</p>
+<p class="copyright">&copy; 2016 Thought At Work. All rights reserved.</p>
+</div>
+</div>
+</footer>
+<script type="text/javascript">
+   $('p').each(function(){
+   var string = $(this).html();
+   string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+   $(this).html(string);
+});
+</script>
+
        <script type="text/javascript" src="js/navScript.js"></script>
 	</body>
 </html>
