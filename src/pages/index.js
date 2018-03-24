85d0ed46c178c4f8b8274ad7f433142756f8f2e6
JacobDFrank <frankjacob42@gmail.com>
2018-03-24 13:51:26 -0400

    Finished footer in react

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Img from "gatsby-image";
+import Footer from './components/Footer'
 
-const IndexPage = ({ data }) => (
+const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div className="footer-sponsors">
-      {data.allFile.edges.map(img => { 
-        return <div className="logo-card"><Img resolutions={img.node.childImageSharp.resolutions} /></div>
-      })}
-    </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <Footer />
   </div>
 )
 
 export default IndexPage
-
-export const query = graphql`
-  query ImagesQuery {
-      # the filter is usefull if you have multiple source-filesystem instances
-      # the name "images" is set in the gatsby-config
-    allFile(filter: {sourceInstanceName: {eq: "images"}}) {
-      edges {
-        node {
-            childImageSharp {
-            # Specify the image processing specifications right in the query.
-            # Makes it trivial to update as your page's design changes.
-            resolutions(width: 125, height: 125) {
-              ...GatsbyImageSharpResolutions
-            }
-          }
-      }
-    }
-  }
-}
-`;
-  
