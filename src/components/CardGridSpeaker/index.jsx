1eee512502c06391c03b605026f9ad0517f5084d
Patrick Ly <patrickly7@yahoo.com>
2018-03-25 19:53:28 -0400

    Created a Card Grid Speaker component and speakers page.

--- src/components/CardGridSpeaker/index.jsx
+++ src/components/CardGridSpeaker/index.jsx
@@ -1,39 +1,36 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import Speaker from './speaker.jsx'
+import data from "../../assets/speakers.json"
 
-const CardGridSpeaker = () => (
-  <div class="speakers__grid-wrap">
-    <div class="speakers__grid grid">
-      <h2 class="speakers__grid__header">speakers</h2>
-      {% for speaker in speakers %}
-        <div class="speaker speakers-card--grid grid__col grid__col--1-of-3 grid__col--m-1-of-2 grid__col--centered">
-          <div class="speakers-card__fields">{{speaker.fields}}</div>
-          <div class="speakers-card__headshot"><img src=""></div>
-          <div class="card">
-            <span class="card__header speakers-card__name">{{speaker.firstName}}
-              {{speaker.lastName}}</span>
-            <span class="card__subhead speakers-card__position">{{speaker.position}}
-              {% if speaker.company != '' %}
-                at {{speaker.company}}
-              {% endif %}
-            </span>
-            <div class="speakers-card__backing-img">
-              <img src="../assets/graphics/TAW-Vis-Dev-6-Edit.jpg">
-            </div>
-          </div>
-          <script type="text/javascript">
-            let fieldString = "{{speaker.fields}}";
-            let fields = fieldString.split(" | ");
-            for(let i = 0; i < fields.length; i++){
-              fields[i] = fields[i].replace(/\s/g, '');
-              fields[i] = fields[i].toLowerCase();
-              $(".speakers-card--grid:last").addClass(fields[i]);
-            }
-          </script>
+class CardGridSpeaker extends React.Component {
+
+  createSpeaker = speaker => {
+    return (
+      <Speaker
+         fields = {speaker.fields}
+        firstName = {speaker.firstName}
+        lastName = {speaker.lastName}
+        position = {speaker.position}
+        company = {speaker.company}
+      />
+    );
+  }
+
+  createSpeakers = speakers => {
+    return speakers.map(this.createSpeaker);
+  }
+
+  render() {
+    return(
+      <div class="speakers__grid-wrap">
+        <div class="speakers__grid grid">
+          <h2 class="speakers__grid__header">speakers</h2>
+          {this.createSpeakers(data.speakers)}
         </div>
-      {% endfor %}
-    </div>
-  </div>
-)
+      </div>
+    );
+  }
+
+}
 
 export default CardGridSpeaker
---
+++ src/pages/speakers.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import CardGridSpeaker from '../components/CardGridSpeaker'
+
+const SpeakerPage = () => (
+  <div>
+    <h1>Speakers Page</h1>
+    <p>Currently under construction</p>
+    <Link to="/">Go back to the homepage</Link>
+    <CardGridSpeaker />
+  </div>
+)
+
+export default SpeakerPage
