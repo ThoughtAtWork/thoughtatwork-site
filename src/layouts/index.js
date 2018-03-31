ccbe160cd560026b4c3f9d06d0d34536caa01fd5
JacobDFrank <frankjacob42@gmail.com>
2018-03-31 14:48:06 -0400

    made page blue

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import './index.css'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+import './index.scss';
 
 const TemplateWrapper = ({ children }) => (
   <div>
