0ca5c09eb5a79e5fc040b207641ed7cdd9e5e303
JacobDFrank <frankjacob42@gmail.com>
2018-03-31 14:47:26 -0400

    Revert "debugging scss file"
    This reverts commit 2cf9a9e6b43b065d4224bb48a7321cac35bd4ab1.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,23 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-
-import './index.scss';
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import './index.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet>
+    <Helmet
       title="Thought At Work 2018"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
-    </Helmet>
+    />
     <Header />
     <div
-  
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
     >
       {children()}
     </div>
