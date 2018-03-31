893d0fef2368e376424b9f3251388d1c0feda6fd
JacobDFrank <frankjacob42@gmail.com>
2018-03-31 16:09:48 -0400

    fixed errors and debugged speakers

--- src/components/CardGridSpeaker/index.jsx
+++ src/components/CardGridSpeaker/index.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import Speaker from './speaker.jsx'
-import data from "../../assets/speakers.json"
+import React from "react";
+import Speaker from "./speaker.jsx";
+import data from "../../assets/speakers.json";
 
 class CardGridSpeaker extends React.Component {
+    createSpeaker = speaker => {
+        return (
+            <Speaker
+                fields={speaker.fields}
+                firstName={speaker.firstName}
+                lastName={speaker.lastName}
+                position={speaker.position}
+                company={speaker.company}
+                key={speaker.lastName}
+            />
+        );
+    };
 
-  createSpeaker = speaker => {
-    return (
-      <Speaker
-         fields = {speaker.fields}
-        firstName = {speaker.firstName}
-        lastName = {speaker.lastName}
-        position = {speaker.position}
-        company = {speaker.company}
-      />
-    );
-  }
+    createSpeakers = speakers => {
+        return speakers.map(this.createSpeaker);
+    };
 
-  createSpeakers = speakers => {
-    return speakers.map(this.createSpeaker);
-  }
-
-  render() {
-    return(
-      <div class="speakers__grid-wrap">
-        <div class="speakers__grid grid">
-          <h2 class="speakers__grid__header">speakers</h2>
-          {this.createSpeakers(data.speakers)}
-        </div>
-      </div>
-    );
-  }
-
+    render() {
+        return (
+            <div className="speakers__grid-wrap">
+                <div className="speakers__grid grid">
+                    <h2 className="speakers__grid__header">speakers</h2>
+                    {this.createSpeakers(data.speakers)}
+                </div>
+            </div>
+        );
+    }
 }
 
-export default CardGridSpeaker
+export default CardGridSpeaker;
