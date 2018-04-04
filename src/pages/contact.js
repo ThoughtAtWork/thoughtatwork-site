b2b0d9a263073630837f6676652232c4a26028e3
JacobDFrank <frankjacob42@gmail.com>
2018-04-03 20:35:23 -0400

    trying to fix contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import CardGridSpeaker from '../components/Contact'
-import Contact from '../components/Contact';
+import React from "react";
+import Link from "gatsby-link";
 
 const ContactPage = () => (
   <div>
     <h1>Speakers Page</h1>
     <p>Currently under construction</p>
     <Link to="/">Go back to the homepage</Link>
-    <Contact />
+    <form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="text" placeholder="Your Name" name="name" />
+      <button>Send</button>
+    </form>
   </div>
-)
+);
 
-export default ContactPage
+export default ContactPage;
