5a173013f295e3e7ea6d08dda9ac79106d0eae1b
JacobDFrank <frankjacob42@gmail.com>
2018-04-03 21:17:38 -0400

    gatsby contact react netlify form look

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,20 +7,19 @@ const ContactPage = () => (
     <p>Currently under construction</p>
     <Link to="/">Go back to the homepage</Link>
 
-   <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name"></input></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email"></input></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+    <form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+    </form>
   </div>
 );
 
