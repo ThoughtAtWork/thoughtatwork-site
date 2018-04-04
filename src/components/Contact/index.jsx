5ef2ff0a453751eb3bc72862b4aca6b8f6f0ce7d
JacobDFrank <frankjacob42@gmail.com>
2018-04-03 20:26:36 -0400

    added in beginning contact form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,36 +1,21 @@
-import React from 'react'
-import Speaker from './speaker.jsx'
-import data from "../../assets/speakers.json"
+import React from "react";
+import Link from "gatsby-link";
 
-class CardGridSpeaker extends React.Component {
-
-  createSpeaker = speaker => {
-    return (
-      <Speaker
-         fields = {speaker.fields}
-        firstName = {speaker.firstName}
-        lastName = {speaker.lastName}
-        position = {speaker.position}
-        company = {speaker.company}
-      />
-    );
-  }
-
-  createSpeakers = speakers => {
-    return speakers.map(this.createSpeaker);
-  }
-
-  render() {
-    return(
-      <div class="speakers__grid-wrap">
-        <div class="speakers__grid grid">
-          <h2 class="speakers__grid__header">speakers</h2>
-          {this.createSpeakers(data.speakers)}
-        </div>
-      </div>
-    );
-  }
-
+class Contact extends React.Component {
+    render() {
+        return (
+            <div className="">
+            <form
+              name="contact"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field">
+                    <input type="text" placeholder="Your Name" name="name" />
+                    <button>Send</button>
+                </form>
+            </div>
+        );
+    }
 }
 
-export default CardGridSpeaker
+export default Contact;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import CardGridSpeaker from '../components/CardGridSpeaker'
+import CardGridSpeaker from '../components/Contact'
+import Contact from '../components/Contact';
 
 const ContactPage = () => (
   <div>
     <h1>Speakers Page</h1>
     <p>Currently under construction</p>
     <Link to="/">Go back to the homepage</Link>
+    <Contact />
   </div>
 )
 
