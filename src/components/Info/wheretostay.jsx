737b1834d0984fc342d2f6c416f8bde0cb102c2f
Patrick Ly <patrickly7@yahoo.com>
2018-04-14 13:37:30 -0400

    Added createLocation and createLocations for the Where to Stay section of the Info page

--- src/components/Info/wheretostay.jsx
+++ src/components/Info/wheretostay.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-
-/*{% for location in stayLocations %}
-<div className="wheretostay_locations">
-	<p className="location__info">{{location.name}}<br />{{location.address}}</p>
-	<p className="location__travel-time">{{location.travelTime}} min drive away from RIT</p>
-	<a href="{{location.website}}" className="location__website">WEBSITE &rarr;</a>
-</div>
-{% endfor %}*/
+import Location from "./location.jsx"
+import data from "../../assets/places_to_stay.json";
 
 class WhereToStay extends React.Component {
+
+    createLocation = location => {
+    	return(
+    		<Location
+    			name={location.name}
+    			address={location.address}
+    			travelTime={location.address}
+    			website={location.website}
+    			key={location.address}
+    		/>
+    	);
+    }
+
+    createLocations = locations => {
+    	return locations.map(this.createLocation);
+    }
+
 	render() {
 		return(
 			<div className="flex wheretostay">
 				<div className="wheretostay_locations_container info-container">
 					<h2 className="wheretostay_header">hotels</h2>
 					
-					/* List of hotel locations go here */
+					{this.createLocations(data.stayLocations)}
 
 					<div className="couch_surfing_block">
 						<p className="couch_surfing"><strong>Couch Surfing</strong></p>
