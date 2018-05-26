39daa0ed31629f011082a4f0f96392543365adf5
Patrick Ly <patrickly7@yahoo.com>
2018-05-26 02:17:32 -0400

    Created new CardKeynoteSpeaker component based off CardGridSpeaker and removed extra text from Speakers page.

---
+++ src/components/CardKeynoteSpeaker/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Speaker from "./keynote_speaker.jsx";
+import data from "../../assets/speakers.json";
+
+class CardKeynoteSpeaker extends React.Component {
+  createSpeaker = speaker => {
+    return (
+      <Speaker
+        fields = {speaker.fields}
+        firstName = {speaker.firstName}
+        lastName = {speaker.lastName}
+        position = {speaker.position}
+        company = {speaker.company}
+        key = {speaker.lastName}
+      />
+    );
+  }
+
+  createSpeakers = speakers => {
+    return speakers.map(this.createSpeaker);
+  }
+
+  render() {
+    return(
+      <div className="speakers__grid-wrap">
+        <div className="speakers__grid grid">
+          <h2 className="speakers__grid__header">speakers</h2>
+          {this.createSpeakers(data.speakers)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CardKeynoteSpeaker;
