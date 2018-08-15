385e536b4c1c200e33c50a2fc19b899cdb0cac5f
JacobDFrank <frankjacob42@gmail.com>
2018-08-15 02:25:02 -0700

    removed and updated gatsby-source-filesystem bulid problem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,21 +6,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
-      // initializing a plugin
-      resolve: 'gatsby-source-filesystem',
-      // giving it a path and name as to where the files will be
-      options: {
-        path: '${__dirname}/src/',
-        name: 'pages',
-      }
-    },
-    'gatsby-plugin-catch-links', {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: '${__dirname}/src/pages',
-        name: 'pages'
-      }
-    }, {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-96615084-2',
