9be668b88395d0ed9a743d83772975d4d710ddd4
JacobDFrank <frankjacob42@gmail.com>
2018-09-18 00:37:48 -0400

    added in fonts

--- src/components/Speaker/SpeakerPhoto.jsx
+++ src/components/Speaker/SpeakerPhoto.jsx
@@ -3,19 +3,15 @@ import classnames from 'classnames';
 import styles from '../../styles/components/speakers/speakerPhoto.module.scss';
 
 let SpeakerPhoto = function statelessFunctionComponentClass(props) {
-  let imageURL = './assets/speaker-headshots/' + 'arnaud_tanielian.jpg';
+  // let imageURL = './assets/speaker-headshots/' + 'arnaud_tanielian.jpg';
   let imageURLPrefix = './assets/speaker-headshots/';
-  let headShotURL = __PATH_PREFIX__ + imageURLPrefix + props.headshot
-    + '.jpg';
 
   return (
     <div>
       <img className={classnames(styles.headshotContainer)}
-        src={headShotURL}
+        src={__PATH_PREFIX__ + imageURLPrefix + props.headshot
+          + '.jpg'}
       />
-      {/* <img className={classnames(styles.headshotContainer)}
-        src={arno}
-      /> */}
       <h2 className={classnames(styles.speakerCard_text__spacing)}>
         {props.firstName + ' ' + props.lastName}
       </h2>
