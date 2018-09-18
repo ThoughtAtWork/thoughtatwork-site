1a215946b07d30f1cceb608b7cd0c15c1cce685f
JacobDFrank <frankjacob42@gmail.com>
2018-09-18 18:45:36 -0400

    trying other console log spot

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,7 +9,9 @@ import favicon from '../assets/images/faviconPurple.png';
 
 
 const TemplateWrapper = ({children}) => (
-  < div className = 'dotGrid-background body-container--padding-top' >
+  console.log('%c Created by Students at RIT, Website lead by Jacob Frank(@jacobdfrank). With design help from Caleb Ng, Amanda Ho, Ben Mendelsohn, Andrew Hurley, and Christine Van. Also with Development help from Shihab Uddin and Patrick', 'background: linear-gradient(#e66465, #9198e5); border: 1px solid #3E0E02; color: white; display: block; text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3); box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 3px 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset; line-height: 30px; text-align: center; font-weight: bold;');
+  console.log('%c Our Github can be found at https://github.com/ThoughtAtWork/thoughtatwork-site', 'background: linear-gradient(#32CD32, #BDB76B); border: 1px solid #3E0E02; color: white; display: block; text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3); box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset; line-height: 40px; text-align: center; font-weight: bold;');
+  <div className = 'dotGrid-background body-container--padding-top' >
     <Helmet
       title="Thought At Work 2018"
       link = {
