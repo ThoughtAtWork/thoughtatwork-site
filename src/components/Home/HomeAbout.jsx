8889fa83151d6ad0bc9a16dbcc13e57515eb5226
JacobDFrank <frankjacob42@gmail.com>
2018-09-18 13:24:10 -0400

    changed myriad to variety

--- src/components/Home/HomeAbout.jsx
+++ src/components/Home/HomeAbout.jsx
@@ -17,7 +17,7 @@ export default class HomeAbout extends Component {
       <div className={classnames('gridish-container--complete container gridish-grid content-Block--margin-top')}>
         <h3 className={classnames('gridish-grid__col--medium--5 gridish-grid__col--small--4 gridish-grid__col--xsmall--3 gridish-grid__height--xsmall--10')}>a design conference made for students by students.</h3>
         <img className={classnames(styles.image)} src={Shapes}/>
-        <h3 className={classnames('gridish-grid__col--medium--5 gridish-grid__col--small--4 gridish-grid__col--xsmall--3')}>we provide a myriad of talks, design workshops, nightly events and more.</h3>
+        <h3 className={classnames('gridish-grid__col--medium--5 gridish-grid__col--small--4 gridish-grid__col--xsmall--3')}>we provide a variety of talks, design workshops, nightly events and more.</h3>
       </div>
     );
   }
