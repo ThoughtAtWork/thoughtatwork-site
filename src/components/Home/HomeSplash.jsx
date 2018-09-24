ca42094978afc223769ce7797037374f2c6d7040
JacobDFrank <frankjacob42@gmail.com>
2018-09-24 17:22:43 -0400

    added in greater details to home and created unset text

--- src/components/Home/HomeSplash.jsx
+++ src/components/Home/HomeSplash.jsx
@@ -18,7 +18,7 @@ export default class HomeSplash extends Component {
       <div className={classnames()}>
         <div className={classnames('container flex flex-column gridish-container--complete gridish-padding--bottom')}>
           <h1 className="flex gridish-padding--bottom">thought at work</h1>
-          <h2 className="flex gridish-grid__height--xsmall--10">october 19 - 21</h2>
+          <h2 className="flex gridish-grid__height--xsmall--10 text-transform--unset">October 19 - 21, 2018</h2>
         </div>
         <SplashAnimation />
       </div>
