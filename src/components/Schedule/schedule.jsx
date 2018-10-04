8e946c23fc224f481d6b302dbd3fe18cbe70a1a2
JacobDFrank <frankjacob42@gmail.com>
2018-10-04 02:56:21 -0400

    finished schedule

--- src/components/Schedule/schedule.jsx
+++ src/components/Schedule/schedule.jsx
@@ -1,46 +1,80 @@
 import React from 'react';
 import classnames from 'classnames';
 import styles from '../../styles/components/schedule/scheduleMain.module.scss';
+import scheduleChevron from '../../assets/images/scheduleChevron.svg';
+import scheduleChevronClosed from '../../assets/images/scheduleChevronClosed.svg';
 
-let schedule = function statelessFunctionComponentClass(props) {
-  let start = props.start.toLowerCase();
-  let end = props.end.toLowerCase();
-  let title = props.title;
-  let presenterLocation = props.presenterLocation;
-  let type = props.type;
-  let description = props.description;
-  let day = props.day;
+class Schedule extends React.Component {
+  constructor(props) {
+    super(props);
 
-  function eventTypeClassName(type) {
-    const prefix = 'event_type-';
+    this.state = {
+      eventOpen: false
+    };
+  }
+
+  onClick = () => {
+    this.setState({ eventOpen: !this.state.eventOpen });
+  }
+
+  eventTypeClassName = (type) => {
+    // const prefix = 'event_type-';
     switch (type) {
-      case 'general':
-        return prefix + 'general';
-      case 'lecture':
-        return prefix + 'lecture';
-      case 'workshop':
-        return prefix + 'workshop';
+      case 'General Event':
+        return '#2071FF';
+      case 'Lecture':
+        return '#FF2350';
+      case 'Workshop':
+        return '#F2C227';
       default:
         return null;
     }
   }
 
-  return (
-    <div className={styles.schedule_block}>
-      <div className={classnames(styles.schedule_block__duration, 'flex flex-align-center')}>
-        <p className={styles.duration}>{start} to {end} {day}</p>
-      </div>
-      <div className={classnames(styles.schedule_block__border)}>
-        <div className={styles.event_info_location}>
-          <h3 className={styles.event_title}>{title}</h3>
-          <p className={styles.presenterLocation}>{presenterLocation}</p>
-        </div>
-        <div className={classnames(styles.event_type, styles.event_margin__bottom)}>
-          <p><span className={eventTypeClassName(type)}>{type}</span>: {description}</p>
-        </div>
-      </div>
-    </div>
-  );
-};
+  render() {
+    let start = this.props.start.toLowerCase();
+    let end = this.props.end.toLowerCase();
+    let title = this.props.title;
+    let presenterLocation = this.props.presenterLocation;
+    let type = this.props.type;
+    let description = this.props.description;
+    let day = this.props.day;
 
-export default schedule;
+    let eventOpen = this.state.eventOpen ? 'block' : 'none';
+    let chevron = this.state.eventOpen ? scheduleChevronClosed : scheduleChevron;
+
+
+    return (
+      <div className={styles.schedule_block}
+        onClick={this.onClick}
+      >
+        <div className={classnames(styles.schedule_block__duration, 'flex flex-align-center')}>
+          <p className={styles.duration}>{start} to {end}, &nbsp;{day}</p>
+        </div>
+        <div
+          className={classnames(styles.schedule_block__content)}
+          style={{ color: this.eventTypeClassName(type) }}
+        >
+          <div className={classnames(styles.event_spacing)}>
+            <div className={classnames('flex flex-justify-between')}
+            >
+              <h3>{title}</h3>
+              <img
+                src={chevron}
+              />
+            </div>
+            <p className={styles.presenterLocation}>{presenterLocation}</p>
+            <div className={classnames(styles.event_description)} style={{ display: eventOpen }}>
+              <p className={classnames(styles.event_description__color)}>
+                <span
+                  style={{ color: this.eventTypeClassName(type) }}
+                >{type}</span>: {description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Schedule;
