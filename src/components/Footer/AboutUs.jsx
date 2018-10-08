11a88451c6d68a9921a6f70777d7d9240311f0a1
JacobDFrank <frankjacob42@gmail.com>
2018-10-08 16:50:03 -0400

    changed caleb's graphic and aobut us footer

--- src/components/Footer/AboutUs.jsx
+++ src/components/Footer/AboutUs.jsx
@@ -8,7 +8,7 @@ export default class AboutUs extends Component {
     return (
       <div className={classnames('container gridish-container gridish-container--complete gridish-grid')}>
         <p className={classnames('gridish-grid__col--small--4 gridish-grid__col--xsmall--4 gridish-grid__height--small--28 gridish-grid__height--xsmall--22', styles.footer_text__maxWidth, styles.footer_text__bottomMargin)}> <strong>Thought At Work</strong> started as a small Industrial Design conference in 2002 but
-         now caters to over 400 designers with speakers and attendees coming various fields in design. Organized exclusively
+         now caters to over 400 designers with speakers and attendees from various fields in design. Organized exclusively
          by students at RIT, the conference strives to function as a tool for young designers to advance their careers
          and thrive.</p>
         <div className={classnames('gridish-grid__col--small--2 gridish-grid__height--small--24 gridish-grid__height--xsmall--2')}></div>
