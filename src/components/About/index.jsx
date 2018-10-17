8f8036f8d541fa1bf1c0d74eb6bd36be1fa2df01
JacobDFrank <frankjacob42@gmail.com>
2018-10-16 21:58:30 -0400

    moved and switched shuffle function

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -5,36 +5,37 @@ import data from '../../assets/about.json';
 import classnames from 'classnames';
 
 class About extends React.Component {
-  render() {
-    var aboutKids = shuffle(data.aboutKids);
-    var aboutLeads = shuffle(data.aboutLeads);
 
-    function shuffle(sourceArray) {
-      for (var i = 0; i < sourceArray.length - 1; i++) {
-        var j = i + Math.floor(Math.random() * (sourceArray.length - i));
-
-        var temp = sourceArray[j];
-        sourceArray[j] = sourceArray[i];
-        sourceArray[i] = temp;
-      }
-      return sourceArray;
+  shuffleArray = (array) => {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
+    return array;
+  }
+
+  render() {
+    var aboutKids = this.shuffleArray(data.aboutKids);
+    var aboutLeads = this.shuffleArray(data.aboutLeads);
     return (
       <div className={classnames()}>
         <Header pageName='about' />
         <div className={classnames(
           'container gridish-container gridish-container--complete')}>
           <h3>history in the making</h3>
-          <br/>
-          <br/>
+          <br />
+          <br />
           <p>
             This conference is entirely run by students. We contact speakers, we craft the schedule, we print the posters, and we produce the title sequence. We meet up year-round, we balance our coursework, we wrap our heads around the big picture, and we stress about the details. Our team changes every year and represents a broad range of disciplines and experiences, but we’re tied together by need to inspire, grow, and teach our community.
-            <br/>
+            <br />
             <br />
           </p>
         </div>
         <div className={'content-Block--margin-top'}>
-          <AboutGrid aboutKids={aboutKids} aboutLeads={aboutLeads}/>
+          <AboutGrid aboutKids={aboutKids} aboutLeads={aboutLeads} />
         </div>
       </div>
     );
