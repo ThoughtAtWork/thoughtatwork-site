e6f5e5e0cba166c137ee4d2844b0bc3bc1ecf13c
JacobDFrank <frankjacob42@gmail.com>
2018-10-27 12:51:40 -0400

    added nav link hover

--- src/components/Home/HomeSplash.jsx
+++ src/components/Home/HomeSplash.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import SplashAnimation from './SplashAnimation';
 import classnames from 'classnames';
-import styles from '../../styles/components/home/homeSplash.module.scss';
-
 
 export default class HomeSplash extends Component {
   constructor(props) {
